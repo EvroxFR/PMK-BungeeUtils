e5dcacbb6e3b7c7689b93cd5e3a618c3d400f132
evroxfr <evroxfr@gmail.com>
2018-09-19 11:04:55 +0200

    /bcall debut

--- src/main/java/fr/pmk_bungeeutils/commands/BroadcastAll.java
+++ src/main/java/fr/pmk_bungeeutils/commands/BroadcastAll.java
@@ -1,6 +1,7 @@
 package fr.pmk_bungeeutils.commands;
 
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class BroadcastAll extends Command {
@@ -11,7 +12,11 @@ public class BroadcastAll extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
+		
+		if(sender instanceof ProxiedPlayer) {
+			ProxiedPlayer player = (ProxiedPlayer) sender;
+			
+		}
 
 	}
 
